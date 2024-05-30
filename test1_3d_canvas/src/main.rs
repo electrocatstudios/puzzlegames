ecfce16b8bc2a0c3da011c4a66805d42a31f0ee8
filtoid <philjeffes@hotmail.com>
2024-05-30 22:05:12 +0200

    screen changing color

---
+++ test1_3d_canvas/src/main.rs
@@ -0,0 +1,33 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+mod game_control;
+mod components;
+mod utils;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+}
+
+fn switch(routes: Route) -> Html {    
+    match routes {
+        Route::Home => html!{
+            <game_control::GameControl />
+        }
+    }
+}
+
+#[function_component(App)]
+fn app_body() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
